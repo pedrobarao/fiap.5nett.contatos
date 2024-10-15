d589ae152bd7aea6f1acb00f3584d713ec00307e
Pedro Barão <8405802+pedrobarao@users.noreply.github.com>
2024-10-15 13:13:19 -0300

    feat: tech challenge fase 2 v1.2.1

--- src/Contatos.Api/Config/PrometheusConfig.cs
+++ src/Contatos.Api/Config/PrometheusConfig.cs
@@ -1,7 +1,9 @@
-﻿using Prometheus;
+﻿using System.Diagnostics.CodeAnalysis;
+using Prometheus;
 
 namespace Contatos.Api.Config;
 
+[ExcludeFromCodeCoverage]
 public static class PrometheusConfig
 {
     public static WebApplication AddPrometheusConfig(this WebApplication app)
