4dbb54f2d9d6e998a87b0c4cd20471930a057f17
Pedro Barão <8405802+pedrobarao@users.noreply.github.com>
2025-10-25 09:09:51 -0300

    feat: incluindo terraform para provisionar aks (#5)
    * feat: adiciona configuração inicial do Terraform para provisionar AKS com baixo custo
    
    * feat: renomeia arquivos de configuração e adiciona workflow de deploy para AKS
    
    * feat: adiciona suporte a execução manual no workflow de build e teste

---
+++ deploy/terraform/provider.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {
+  }
+  resource_provider_registrations = "none"
+  subscription_id                 = "5c748bbc-ffa0-41df-a518-5f396262b1b2"
+  environment                     = "public"
+  use_msi                         = false
+  use_cli                         = true
+  use_oidc                        = false
+}
