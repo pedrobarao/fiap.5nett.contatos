92866b38383e93d0787570da851af9c2c2bad6bc
Pedro Barão <8405802+pedrobarao@users.noreply.github.com>
2025-10-25 12:43:21 -0300

    feat: adiciona script de aplicação de manifests do Kubernetes

---
+++ deploy/k8s/apply.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+echo "Criando recursos Kubernetes..."
+
+echo "Criando namespace..."
+kubectl apply -f deploy/k8s/namespace.yaml
+
+echo "Aplicando Metrics Server..."
+kubectl apply -f deploy/k8s/metrics-server.yaml
+
+echo "Criando Secrets..."
+kubectl apply -f deploy/k8s/secrets.yaml
+
+echo "Criando PVCs..."
+kubectl apply -f deploy/k8s/rabbitmq/rabbitmq-pvc.yaml
+kubectl apply -f deploy/k8s/cadastro-api/postgres-pvc.yaml
+kubectl apply -f deploy/k8s/consulta-api/mongodb-pvc.yaml
+kubectl apply -f deploy/k8s/prometheus/prometheus-pvc.yaml
+kubectl apply -f deploy/k8s/grafana/grafana-pvc.yaml
+
+echo "Criando ConfigMaps..."
+kubectl apply -f deploy/k8s/prometheus/prometheus-config.yaml
+kubectl apply -f deploy/k8s/grafana/grafana-config.yaml
+
+echo "Criando Deployments e Services..."
+kubectl apply -f deploy/k8s/cadastro-api/postgres.yaml
+kubectl apply -f deploy/k8s/cadastro-api/postgres-service.yaml
+kubectl apply -f deploy/k8s/consulta-api/mongodb.yaml
+kubectl apply -f deploy/k8s/consulta-api/mongodb-service.yaml
+kubectl apply -f deploy/k8s/rabbitmq/rabbitmq.yaml
+kubectl apply -f deploy/k8s/rabbitmq/service.yaml
+kubectl apply -f deploy/k8s/prometheus/prometheus.yaml
+kubectl apply -f deploy/k8s/prometheus/service.yaml
+kubectl apply -f deploy/k8s/grafana/grafana.yaml
+kubectl apply -f deploy/k8s/grafana/service.yaml
+kubectl apply -f deploy/k8s/cadastro-api/cadastro-api.yaml
+kubectl apply -f deploy/k8s/cadastro-api/service.yaml
+kubectl apply -f deploy/k8s/consulta-api/consulta-api.yaml
+kubectl apply -f deploy/k8s/consulta-api/service.yaml
+kubectl apply -f deploy/k8s/cadastro-api/hpa.yaml
+kubectl apply -f deploy/k8s/consulta-api/hpa.yaml
+
+echo "Aguardando pods iniciarem..."
+kubectl wait --namespace contatos-app \
+  --for=condition=ready pod \
+  --selector=app=rabbitmq \
+  --timeout=90s
+kubectl wait --namespace contatos-app \
+  --for=condition=ready pod \
+  --selector=app=postgres \
+  --timeout=90s
+kubectl wait --namespace contatos-app \
+  --for=condition=ready pod \
+  --selector=app=mongodb \
+  --timeout=90s
+kubectl wait --namespace contatos-app \
+  --for=condition=ready pod \
+  --selector=app=cadastro-contatos-api \
+  --timeout=90s
+kubectl wait --namespace contatos-app \
+  --for=condition=ready pod \
+  --selector=app=consulta-contatos-api \
+  --timeout=90s
+
+echo "Verificando recursos criados..."
+kubectl get all -n contatos-app
+
+echo "Aplicação implantada com sucesso!" 
