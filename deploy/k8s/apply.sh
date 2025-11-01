99bcaf094b33c67da074ff397f3448d756b86512
Pedro Barão <8405802+pedrobarao@users.noreply.github.com>
2025-11-01 15:55:55 -0300

    chore: ajusta a aplicação para a apresentação

--- deploy/k8s/apply.sh
+++ deploy/k8s/apply.sh
@@ -19,7 +19,7 @@ kubectl apply -f deploy/k8s/cadastro-api/svc-api.yaml
 kubectl apply -f deploy/k8s/consulta-api/svc-api.yaml
 
 echo "Criando Ingress..."
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller/v1.10.1/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f deploy/k8s/ingress.yaml
 
 echo "Aplicando Metrics Server..."
