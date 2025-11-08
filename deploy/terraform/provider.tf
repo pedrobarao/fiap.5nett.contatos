c463799407e6113084768c47d0b4faad35e12fd2
Pedro Barão <8405802+pedrobarao@users.noreply.github.com>
2025-11-08 12:18:45 -0300

    chore: adiciona comentário explicativo sobre o uso das credenciais do AZ CLI

--- deploy/terraform/provider.tf
+++ deploy/terraform/provider.tf
@@ -5,6 +5,6 @@ provider "azurerm" {
   subscription_id                 = "5c748bbc-ffa0-41df-a518-5f396262b1b2"
   environment                     = "public"
   use_msi                         = false
-  use_cli                         = true
+  use_cli                         = true # Utiliza as credenciais do AZ CLI
   use_oidc                        = false
 }
